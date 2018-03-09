49a82e1a00c1fbfe5f06a0bce4822112e753fa9c
n-a-n <loic.truchot35@gmail.com>
2018-03-09 07:28:13 +0100

    first commit

---
+++ core-javascript.js
@@ -0,0 +1,36 @@
+// the language that no one learn
+// c/p a lot of stuff, relay on stackoverflow
+
+// then misunderstanding leads to hate & language blaming
+if (a === 1 && a === 2 && a === 3) {
+  // prototype
+  console.log("JavaScript is fucked up");
+}
+if ([] + [] === "") {
+  // coercion
+  console.log("WTF JavaScript...");
+}
+if (NaN !== NaN) {
+  // decision
+  console.log("My weird cousin JavaScript");
+}
+if (typeof null === "object") {
+  // real lack
+  console.log("OK, sometimes, it's true");
+}
+
+// a brief history of JS
+
+// scope, compiler & runtime + LHS/RHS
+
+// eventloop
+
+// scope -> closure -> modules
+
+// native types and their constructors + TypeError
+
+// coercion & equality
+
+// undefined vs undeclared + ReferenceError
+
+// use strict, eval & with
