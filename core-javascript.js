cb3a17d98b90ac6ddf04bb57d33c5ae9cdd3775f
n-a-n <loic.truchot35@gmail.com>
2018-03-11 14:21:56 +0100

    level 1 reordered

--- core-javascript.js
+++ core-javascript.js
@@ -1,36 +1,56 @@
-// the language that no one learn
-// c/p a lot of stuff, relay on stackoverflow
+// ****************************************
+// ****** FOREWORDS ABOUT JAVASCRIPT ******
 
+// THE LANGUAGE THAT NO ONE LEARN
+// begin with some web project, whitout any training
+// use framwork/lib with getstarted tuto, c/p a lot of stuff, relay on stackoverflow
 // then misunderstanding leads to hate & language blaming
-if (a === 1 && a === 2 && a === 3) {
-  // prototype
-  console.log("JavaScript is fucked up");
-}
-if ([] + [] === "") {
-  // coercion
-  console.log("WTF JavaScript...");
-}
-if (NaN !== NaN) {
-  // decision
-  console.log("My weird cousin JavaScript");
-}
-if (typeof null === "object") {
-  // real lack
-  console.log("OK, sometimes, it's true");
+var x = 1;
+var a = x === 1 && x === 2 && x === 3,
+  b = [] + [] === "",
+  c = NaN !== NaN,
+  d = typeof null === "object",
+  e = new Boolean(false);
+if (a && b && c && d && e) {
+  console.log("JavaScript seems so fucked up");
 }
 
-// a brief history of JS
+// A BRIEF HISTORY OF JAVASCRIPT
 
-// scope, compiler & runtime + LHS/RHS
+// JAVASCRIPT PHILOSOPHERS
+// Existence comes before being; J.P. Sartre
+// Mix Java + C syntaxe and behaviors
+// intrepreted script (for HTTP purpose) vs compiled executable
+// without try / catch madness (aka coercion)
+// without class requirement (aka composition vs inheritance)
 
-// eventloop
+// **************************************************
+// ****** CREATE A MODULAR QUIZZ PROCESS IN JS ******
 
-// scope -> closure -> modules
+// TYPES
+// primitive types and their boxing behavior
 
-// native types and their constructors + TypeError
+// SCOPE
+// function is the only scope
+// interpreter/compiler & runtime + LHS/RHS
+// undefined vs undeclared + ReferenceError
+
+// CLOSURE
+for (var timer = 1; timer <= 5; timer++) {
+  setTimeout(function() {
+    console.log("Elapsed second: " + timer);
+  }, 1000 * timer);
+}
+
+// falsy vs truthy values
+var falsy = [undefined, null, false, 0, NaN, ""]; // that's all folks !
 
 // coercion & equality
 
-// undefined vs undeclared + ReferenceError
-
 // use strict, eval & with
+
+// eventloop
+
+// The ES new feature process
+
+// the big frameworks and library
